9421888f02c3b0959a1ee1baf20f855cb565e191
manighahrmani <mashapicasso@gmail.com>
2023-08-18 17:40:06 +0100

    Add gaming laptop

--- pract11.py
+++ pract11.py
@@ -32,6 +32,56 @@ class Laptop:
         output += " priced at £{}".format(self.getPrice())
         return output
 
+class GamingLaptop(Laptop):
+    gpuOptions = {
+        "NVIDIA GTX 1650": 100,
+        "NVIDIA GTX 1660 Ti": 150,
+        "NVIDIA RTX 3070": 250,
+        "NVIDIA RTX 4080": 350,
+        "AMD RX 5700M": 180,
+        "AMD RX 6800M": 280
+    }
+
+    def __init__(self, brand, basePrice):
+        super().__init__(brand, basePrice)
+        self.gpu = "NVIDIA GTX 1650"
+
+    def getGPU(self):
+        return self.gpu
+
+    def setGPU(self, gpu):
+        if gpu in self.gpuOptions:
+            self.gpu = gpu
+            self.basePrice += self.gpuOptions[gpu]
+
+    def getPrice(self):
+        priceOfRam = self.ramOptions[self.ram]
+        priceOfGPU = self.gpuOptions[self.gpu]
+        price = self.basePrice + priceOfRam + priceOfGPU
+        return price
+
+    def __str__(self):
+        output = "{} Gaming Laptop with {} GB RAM and {}".format(
+            self.brand, self.ram, self.gpu)
+        output += " GPU priced at £{}".format(self.getPrice())
+        return output
+
+def testGamingLaptop():
+    gaming_laptop = GamingLaptop("Alienware", 1499.99)
+    print("The {} gaming laptop with {} GB RAM and {} GPU costs £{}".format(
+        gaming_laptop.getBrand(), gaming_laptop.getRam(),
+        gaming_laptop.getGPU(), gaming_laptop.getPrice()))
+
+    gaming_laptop.setRam(32)
+    gaming_laptop.setGPU("NVIDIA RTX 3070")
+    print("The RAM is now {} and the GPU is {}.".format(
+        gaming_laptop.getRam(), gaming_laptop.getGPU()))
+
+    print(gaming_laptop)
+
+testGamingLaptop()
+
+
 def testLaptop():
     laptop = Laptop("Dell", 999.99)
     print("The {} laptop with {} GB RAM costs £{}".format(
@@ -42,5 +92,3 @@ def testLaptop():
         laptop.getRam(), laptop.getPrice()))
 
     print(laptop)
-
-testLaptop()
