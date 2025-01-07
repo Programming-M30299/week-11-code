b0c92a48e19fbfe85f46e67a5c728e946175bb43
Mani Ghahremani <mashapicasso@gmail.com>
2025-01-07 01:52:40 +0000

    Update the lecture code per new slides

--- lect11.py
+++ lect11.py
@@ -1,104 +1,113 @@
 class Pizza:
 
-    pizzaPrices = {"small": 8, "medium": 10, "large": 12}
+    valid_toppings = {'cheese', 'pepperoni',
+                      'mushrooms', 'olives'}
 
     def __init__(self, size):
-        self.toppings = set()
-        self.size = "small"
-        if size.lower() in self.pizzaPrices:
-            self.size = size.lower()
+        self.size = size
+        self._toppings = set()
 
-    def addTopping(self, topping):
-        self.toppings.add(topping.lower())
+    @property
+    def toppings(self):
+        return self._toppings
 
-    def removeTopping(self, topping):
-        self.toppings.remove(topping.lower())
-
-    def setSize(self, newSize):
-        if newSize.lower() in self.pizzaPrices:
-            self.size = newSize.lower()
-
-    def getSize(self):
-        return self.size
-
-    def getPrice(self):
-        return self.pizzaPrices[self.size]
-
-    def getToppings(self):
-        return self.toppings
+    def add_topping(self, topping):
+        if topping in self.valid_toppings:
+            self._toppings.add(topping)
 
     def __str__(self):
-        output = f"{self.size} pizza with:"
+        output = f"A {self.size} pizza with the following toppings:"
         for topping in self.toppings:
             output += f"\n- {topping}"
-        output += f"\nPrice: £{self.getPrice()}\n"
-        return output
-
-
-class Order:
-
-    def __init__(self):
-        self.pizzas = []
-
-    def addPizza(self, pizza):
-        self.pizzas.append(pizza)
-
-    def getTotalPrice(self):
-        totalPrice = 0
-        for pizza in self.pizzas:
-            totalPrice += pizza.getPrice()
-        return totalPrice
-
-    def __str__(self):
-        output = "Your order contains:\n"
-        for pizza in self.pizzas:
-            output += str(pizza)
-        output += f"Total: £{self.getTotalPrice()}"
         return output
 
 
 class StuffedCrustPizza(Pizza):
-    
-    crustTypes = ("cheese", "garlic", "hot dog")
 
     def __init__(self, size, crust):
         super().__init__(size)
-        self.crust = "cheese"
-        if crust.lower() in self.crustTypes:
-            self.crust = crust.lower()
+        self._crust = crust
 
-    def getCrust(self):
-        return self.crust
+    @property
+    def crust(self):
+        return self._crust
 
-    def setCrust(self, newCrust):
-        if newCrust in self.crustTypes:
-            self.crust = newCrust
-
-    def getPrice(self):
-        return super().getPrice() + 2
+    @crust.setter
+    def crust(self, new_crust):
+        if new_crust == 'mozzarella' or new_crust == 'hot dog':
+            self._crust = new_crust
 
     def __str__(self):
-        output = f"A {self.size} stuffed crust pizza with:"
+        output = f"A {self.size} stuffed crust pizza"
+        output += f" with {self.crust} crust and the following toppings:"
         for topping in self.toppings:
             output += f"\n- {topping}"
-        output += f"\nCrust type: {self.crust}"
-        output += f"\nPrice: £{self.getPrice()}\n"
         return output
 
 
-def test():
-    myOrder = Order()
-    pizza1 = Pizza("small")
-    pizza1.addTopping("Pepperoni")
-    pizza1.addTopping("Jalapenos")
-    pizza1.setSize("large")
+class PizzaOrder:
 
-    pizza2 = StuffedCrustPizza("Medium", "Cheese")
-    pizza2.addTopping("Ham")
-    pizza2.addTopping("Mushrooms")
-    pizza2.setCrust("Garlic")
+    def __init__(self):
+        self.pizzas = []
 
-    myOrder.addPizza(pizza1)
-    myOrder.addPizza(pizza2)
+    def add_pizza(self, pizza):
+        self.pizzas.append(pizza)
 
-    print(myOrder)
+    def remove_pizza(self, index):
+        if 0 <= index < len(self.pizzas):
+            self.pizzas.pop(index)
+
+    def __str__(self):
+        output = "Your order contains:"
+        num_pizzas = len(self.pizzas)
+        for i in range(num_pizzas):
+            pizza = self.pizzas[i]
+            index = i + 1
+            output += f"\n{index}. {pizza}"
+        return output
+
+
+def test_pizza():
+    pizza = Pizza("large")
+    pizza.size = "medium"
+    # pizza.toppings = {'cheese', 'pepperoni'} # AttributeError
+    pizza.add_topping("cheese")
+    pizza.add_topping("pepperoni")
+
+    print(pizza)
+
+
+def test_stuffed_crust_pizza():
+    pizza = StuffedCrustPizza("large", "mozzarella")
+    print(pizza.toppings)
+    pizza.size = "medium"
+    pizza.add_topping("cheese")
+    pizza.add_topping("pepperoni")
+    pizza.crust = 'hot dog'
+    pizza.crust = 'cheddar'  # should not change the crust
+
+    print(pizza)
+
+
+def test_pizza_order():
+    order = PizzaOrder()
+
+    pizza1 = Pizza("large")
+    pizza1.add_topping("cheese")
+    pizza1.add_topping("pepperoni")
+    order.add_pizza(pizza1)
+
+    pizza2 = StuffedCrustPizza("medium", "mozzarella")
+    pizza2.add_topping("cheese")
+    pizza2.add_topping("mushrooms")
+    pizza2.crust = 'mozzarella'
+    order.add_pizza(pizza2)
+
+    print(order)
+    print()
+
+    order.remove_pizza(0)
+    print(order)
+    print()
+
+    order.remove_pizza(1)  # should not remove anything
